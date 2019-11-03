7ecef431395920f5c3c2e38676701e09af2663f4
astic1764 <astic1764@gmail.com>
2019-11-03 19:03:15 +0900

    Updates

---
+++ precache-manifest.d250cda14c85efee6f5d9b8f9a5d6be5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d03b5995840596f032beb088a89e55",
+    "url": "/semi-full-todolist/index.html"
+  },
+  {
+    "revision": "cc615a300e095539f8c4",
+    "url": "/semi-full-todolist/static/js/2.27a61c2f.chunk.js"
+  },
+  {
+    "revision": "e8a5142a584bff1a654d",
+    "url": "/semi-full-todolist/static/js/main.bd21c0e4.chunk.js"
+  },
+  {
+    "revision": "ff691b1ee16c4adb5a12",
+    "url": "/semi-full-todolist/static/js/runtime-main.20653cd4.js"
+  }
+]);
